8c39cd4409a0946f346b95ff4678582c2052b0ba
M16-030B\E460 <rizky.eko.putra@gmail.com>
2018-02-27 14:29:06 +0700

    adding flag
    adding flag to mark that a boom

--- MineSweeper/Program.cs
+++ MineSweeper/Program.cs
@@ -17,6 +17,7 @@ namespace MineSweeper
             string input = "y";
             while (input == "y")
             {
+                Console.Clear();
                 Console.WriteLine("=====Mine Sweeper=====");
                 Console.WriteLine();
                 flag = false;
@@ -30,12 +31,20 @@ namespace MineSweeper
                 {
                     Console.WriteLine();
                     Console.WriteLine();
+                    Console.Write("assign a flag(y/n): ");
+                    string flagBoom = Console.ReadLine().ToLower();
                     Console.Write("input x (space) y: ");
                     string[] xy = Console.ReadLine().Split(' ');
                     int y = Convert.ToInt32(xy[0]);
                     int x = Convert.ToInt32(xy[1]);
-                    cekMap(map, temp, size, x, y, out result);
+                    if (temp[x, y] != " " && temp[x, y] != "X" && temp[x, y] != "F")
+                    {
+                        int countFlagBoom = checkFlagBoomAround(temp, map, x, y, size);
+                        if (countFlagBoom > 9) flag = true;
+                        else if (countFlagBoom == map[x, y] && countFlagBoom < 10) cekFlagMap(map, temp, size, x, y, result);
+                    } else cekMap(map, temp, size, x, y, flagBoom, out result);
                     Console.WriteLine("======================");
+                    Console.Clear();
                     printMineMap(result, size);
                     if (win)
                     {
@@ -134,7 +143,7 @@ namespace MineSweeper
                 Console.Write(i + "");
                 for (int j = 0; j < size; j++)
                 {
-                    if (result[i, j] == " ") count++;
+                    if (result[i, j] == " " || result[i, j] == "F") count++;
                     Console.Write('|' + result[i, j]);
                 }
                 Console.Write('|');
@@ -149,38 +158,77 @@ namespace MineSweeper
             if (count == 10) win = true;
         }
 
-        static void cekMap(int[,] map, string[,] temp, int size, int x, int y, out string[,] result)
+        static void cekMap(int[,] map, string[,] temp, int size, int x, int y, string flagBoom, out string[,] result)
         {
-            if (temp[x, y] == "X")
+            if (flagBoom == "y")
             {
+                if (temp[x, y] != " " && temp[x, y] != "F") { }
+                else if (temp[x, y] == " ") temp[x, y] = "F";
+                else temp[x, y] = " ";
                 result = temp;
-            }
-            else if (map[x, y] == 99)
-            {
-                temp[x, y] = "B";
-                flag = true;
-            }
-            else if (map[x, y] > 0)
-            {
-                temp[x, y] = map[x, y].ToString();
             } else
             {
-                temp[x, y] = "X";
-                for (int i = x - 1; i <= x + 1; i++)
+                if (temp[x, y] == "F") { }
+                else if (temp[x, y] == "X") result = temp;
+                else if (map[x, y] == 99)
                 {
-                    for (int j = y - 1; j <= y + 1; j++)
+                    temp[x, y] = "B";
+                    flag = true;
+                }
+                else if (map[x, y] > 0)
+                {
+                    temp[x, y] = map[x, y].ToString();
+                }
+                else
+                {
+                    temp[x, y] = "X";
+                    for (int i = x - 1; i <= x + 1; i++)
                     {
-                        if ((i == x && j == y) || i >= size || i < 0 || j >= size || j < 0 || map[i, j] == 99)
+                        for (int j = y - 1; j <= y + 1; j++)
                         {
-                            continue;
-                        } else
-                        {
-                            cekMap(map, temp, size, i, j, out result);
+                            if ((i == x && j == y) || i >= size || i < 0 || j >= size || j < 0 || map[i, j] == 99)
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                cekMap(map, temp, size, i, j, flagBoom, out result);
+                            }
                         }
                     }
                 }
+                result = temp;
             }
-            result = temp;
+        }
+
+        static int checkFlagBoomAround(string[,] temp, int[,] map, int x, int y, int size)
+        {
+            int count = 0;
+            for (int i = x - 1; i <= x + 1; i++)
+            {
+                for (int j = y - 1; j <= y + 1; j++)
+                {
+                    if ((i == x && j == y) || i >= size || i < 0 || j >= size || j < 0) continue;
+                    else if (temp[i, j] == "F")
+                    {
+                        if (map[i, j] == 99) count++;
+                        else count += 100;
+                    }
+                }
+            }
+            return count;
+        }
+
+        static void cekFlagMap(int[,] map, string[,] temp, int size, int x, int y, string[,] result)
+        {
+            for (int i = x - 1; i <= x + 1; i++)
+            {
+                for (int j = y - 1; j <= y + 1; j++)
+                {
+                    if ((i == x && j == y) || i >= size || i < 0 || j >= size || j < 0) continue;
+                    else if (temp[i, j] == " ") cekMap(map, temp, size, i, j, "n", out result);
+                }
+            }
         }
     }
 }
